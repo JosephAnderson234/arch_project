4989fa8cbe54dc617cca7c8900f1a79aa25f5b92
JosephAnderson234 <joseph.cose@utec.edu.pe>
2025-10-07 10:54:21 -0500

    feat: structured files created for the design

---
+++ Arch_Project.srcs/sources_1/new/fp_alu_top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.10.2025 10:39:53
+// Design Name: 
+// Module Name: fp_alu_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fp_alu_top(
+
+    );
+endmodule
---
+++ Arch_Project.srcs/sources_1/new/src/fpu_muldiv.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.10.2025 10:52:42
+// Design Name: 
+// Module Name: fpu_muldiv
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fpu_muldiv(
+
+    );
+endmodule
